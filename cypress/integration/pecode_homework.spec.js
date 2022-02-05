bc3a267e393ebf9d5da8d24bfa70cb5a6ad5e2ff
Sergey <semenenkos531@gmail.com>
2022-02-05 15:54:57 +0200

    V1.1 Another description of the steps

--- cypress/integration/pecode_homework.spec.js
+++ cypress/integration/pecode_homework.spec.js
@@ -6,31 +6,31 @@ describe('Filling out the "Text box" form', () => {
         cy.visit(MAIN_URL);
     });
 
-    it('Check that the user can enter a valid name in the "Full name" field.', () => {
+    it('Entering a valid name in the "Full name" field.', () => {
         cy.get(locators.USER_NAME_LOCATOR)
             .type(RANDOM_NAME)
             .should('have.value', RANDOM_NAME);
     });
 
-    it('Check that the user can enter a valid email in the "Email" field.', () => {
+    it('Entering a valid email in the "Email" field.', () => {
         cy.get(locators.USER_EMAIL_LOCATOR)
             .type(RANDOM_EMAIL)
             .should('have.value', RANDOM_EMAIL);
     });
 
-    it('Check that the user can enter a valid сurrent address in the "Current adress" field.', () => {
+    it('Entering a valid сurrent address in the "Current adress" field.', () => {
         cy.get(locators.CURRENT_ADRESS_LOCATOR)
             .type(RANDOM_ADDRESS)
             .should('have.value', RANDOM_ADDRESS);
     });
 
-    it('Check that the user can enter a valid permanent address in the "Permanent Address" field.', () => {
+    it('Entering a valid permanent address in the "Permanent Address" field.', () => {
         cy.get(locators.PERMANENT_ADRESS_LOCATOR)
             .type(RANDOM_ADDRESS)
             .should('have.value', RANDOM_ADDRESS);
     });
 
-    it('Check that the user can click on the "Submit" button.', () => {
+    it('Clicking on the "Submit" button.', () => {
         cy.get(locators.SUBMIT_BUTTON_LOCATOR)
             .click();
     });
@@ -40,5 +40,6 @@ describe('Filling out the "Text box" form', () => {
         cy.get(locators.OUTPUT_USER_EMAIL_LOCATOR).should('include.text', RANDOM_EMAIL);
         cy.get(locators.OUTPUT_CURRENT_ADRESS_LOCATOR).should('include.text', RANDOM_ADDRESS);
         cy.get(locators.OUTPUT_PERMANENT_ADRESS_LOCATOR).should('include.text', RANDOM_ADDRESS);
+        cy.screenshot();
     });
 });

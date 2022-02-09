3a3aa2379d1d80539452e93b1a1de03cfc76e502
Borutskyi Sergey <74474277+CyberSergo@users.noreply.github.com>
2022-02-09 21:24:56 +0200

    Delete cypress directory

--- cypress/modules/variables.mjs
+++
@@ -1,8 +0,0 @@
-const { faker } = require('@faker-js/faker');
-
-export const MAIN_URL = 'https://demoqa.com/text-box';
-
-export const RANDOM_NAME = faker.name.findName();
-export const RANDOM_EMAIL = faker.internet.email();
-export const RANDOM_ADDRESS = faker.address.secondaryAddress();
-

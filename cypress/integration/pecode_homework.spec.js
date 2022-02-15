780b3d70a4c75cedf14ce7f48d1cabb12dca57e4
Sergey <semenenkos531@gmail.com>
2022-02-16 00:00:04 +0200

    version after review

--- cypress/integration/pecode_homework.spec.js
+++ cypress/integration/pecode_homework.spec.js
@@ -18,14 +18,14 @@ describe('Filling out the "Text box" form', () => {
             .should('have.value', RANDOM_EMAIL);
     });
 
-    it('Entering a valid сurrent address in the "Current adress" field.', () => {
-        cy.get(locators.CURRENT_ADRESS_LOCATOR)
+    it('Entering a valid сurrent address in the "Current address" field.', () => {
+        cy.get(locators.CURRENT_ADDRESS_LOCATOR)
             .type(RANDOM_ADDRESS)
             .should('have.value', RANDOM_ADDRESS);
     });
 
     it('Entering a valid permanent address in the "Permanent Address" field.', () => {
-        cy.get(locators.PERMANENT_ADRESS_LOCATOR)
+        cy.get(locators.PERMANENT_ADDRESS_LOCATOR)
             .type(RANDOM_ADDRESS)
             .should('have.value', RANDOM_ADDRESS);
     });
@@ -38,8 +38,8 @@ describe('Filling out the "Text box" form', () => {
     it('Checking the correctness of all data ', () => {
         cy.get(locators.OUTPUT_USER_NAME_LOCATOR).should('include.text', RANDOM_NAME);
         cy.get(locators.OUTPUT_USER_EMAIL_LOCATOR).should('include.text', RANDOM_EMAIL);
-        cy.get(locators.OUTPUT_CURRENT_ADRESS_LOCATOR).should('include.text', RANDOM_ADDRESS);
-        cy.get(locators.OUTPUT_PERMANENT_ADRESS_LOCATOR).should('include.text', RANDOM_ADDRESS);
+        cy.get(locators.OUTPUT_CURRENT_ADDRESS_LOCATOR).should('include.text', RANDOM_ADDRESS);
+        cy.get(locators.OUTPUT_PERMANENT_ADDRESS_LOCATOR).should('include.text', RANDOM_ADDRESS);
         cy.screenshot();
     });
 });
